aacf02c200346324e6734e208bd5372dbce739dc
DeathPussy <89157788+DeathPussy@users.noreply.github.com>
2021-09-24 14:48:05 +0300

    Create learm_js_1.js

---
+++ learm_js_1.js
@@ -0,0 +1,13 @@
+let name = "<h1>Мариночка<3</h1>";
+let age = 12;
+
+document.write(name);
+alert("Маря❤");
+
+if (age == 12){
+	document.write("Мариночке 12 годиков<3<br>");
+}
+
+else{
+document.write("Вы не угадали сколько годиков Мариночке<3");
+}
